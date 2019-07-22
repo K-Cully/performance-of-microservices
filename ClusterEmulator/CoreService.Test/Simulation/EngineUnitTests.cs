7e6b55c979ec7c48acbfc8f52f8f20ef811a402c
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-07-22 20:35:29 +0100

    AB#35 Add more UTs

--- ClusterEmulator/CoreService.Test/Simulation/EngineUnitTests.cs
+++ ClusterEmulator/CoreService.Test/Simulation/EngineUnitTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
+using System.Collections.Generic;
 
 namespace CoreService.Test
 {
@@ -45,7 +46,7 @@ namespace CoreService.Test
         [TestMethod]
         [TestCategory("Functional")]
         [ExpectedException(typeof(NullReferenceException))]
-        public void ProcessRequest_Throws_WhenRegistryReturnsNull()
+        public void ProcessRequest_Throws_WhenProcessorIsNull()
         {
             string name = "test";
             Mock<IRegistry> registryMock = new Mock<IRegistry>(MockBehavior.Strict);
@@ -54,5 +55,58 @@ namespace CoreService.Test
 
             ActionResult<string> result = engine.ProcessRequest(name);
         }
+
+
+        [TestMethod]
+        [TestCategory("Functional")]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ProcessRequest_Throws_WhenStepIsNull()
+        {
+            string name = "test";
+            Mock<IRegistry> registryMock = new Mock<IRegistry>(MockBehavior.Strict);
+            IProcessor processor = defaultProcessor;
+            processor.Steps = new List<string> { null };
+            processor.Name = name;
+
+            registryMock.Setup(reg => reg.GetProcessor(name))
+                .Returns<string>((n) => processor);
+            registryMock.Setup(reg => reg.GetStep(It.IsAny<string>()))
+                .Returns<string>(null);
+            Engine engine = new Engine(registryMock.Object);
+
+            ActionResult<string> result = engine.ProcessRequest(name);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Functional")]
+        public void ProcessRequest_ExecutesSuccessfully_WhenStepsAreSuccessful()
+        {
+            string name = "test";
+            Mock<IRegistry> registryMock = new Mock<IRegistry>(MockBehavior.Strict);
+            IProcessor processor = defaultProcessor;
+            processor.Steps = new List<string> { "step1" };
+            processor.Name = name;
+
+            // TODO: return valid step
+            registryMock.Setup(reg => reg.GetProcessor(name))
+                .Returns<string>((n) => processor);
+            registryMock.Setup(reg => reg.GetStep(It.IsAny<string>()))
+                .Returns<string>(null);
+            Engine engine = new Engine(registryMock.Object);
+
+            // TODO: verify result
+            ActionResult<string> result = engine.ProcessRequest(name);
+        }
+
+
+        private readonly Processor defaultProcessor = new Processor
+            {
+                Name = string.Empty,
+                Steps = new List<string>(),
+                ErrorPayloadSize = 0,
+                SuccessPayloadSize = 0,
+                IngressLatencyMilliseconds = 0
+            };
     }
 }
