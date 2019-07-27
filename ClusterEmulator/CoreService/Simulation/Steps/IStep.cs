12e381a40c288c578654976669768c6f0346eb6b
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-07-27 15:01:57 +0100

    AB#40 Remove name and add load to load step

--- ClusterEmulator/CoreService/Simulation/Steps/IStep.cs
+++ ClusterEmulator/CoreService/Simulation/Steps/IStep.cs
@@ -5,10 +5,6 @@ namespace CoreService.Simulation.Steps
 {
     public interface IStep
     {
-        // TODO: restrict
-        string Name { get; set; }
-
-
         Task<ExecutionStatus> ExecuteAsync();
     }
 }
--- ClusterEmulator/CoreService/Simulation/Steps/LoadStep.cs
+++ ClusterEmulator/CoreService/Simulation/Steps/LoadStep.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,16 +11,43 @@ namespace CoreService.Simulation.Steps
     {
         // TODO
 
-        public string Name
-        {
-            get;
-            set;
-        }
-
         public async Task<ExecutionStatus> ExecuteAsync()
         {
             // TODO
-            return await Task.Run(() => ExecutionStatus.Success);
+
+            // TODO: validate and get from settings
+            int seconds = 5;
+            int percentage = 50;
+
+            List<Task> coreTasks = new List<Task>();
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                coreTasks.Add(GenerateLoad(seconds, percentage));
+            }
+
+            await Task.WhenAll(coreTasks);
+            return ExecutionStatus.Success;
+        }
+
+
+        private async Task GenerateLoad(int seconds, int percentage)
+        {
+            Stopwatch watch = new Stopwatch();
+
+            long runtime = (long)seconds * 1000;
+            long elapsed = 0;
+            watch.Start();
+            while (elapsed < runtime)
+            {
+                // Generate load for the target percentage, sleep for the remaining time
+                if (watch.ElapsedMilliseconds > percentage)
+                {
+                    elapsed += watch.ElapsedMilliseconds;
+                    await Task.Delay(100 - percentage).ConfigureAwait(false);
+                    watch.Reset();
+                    watch.Start();
+                }
+            }
         }
     }
 }
