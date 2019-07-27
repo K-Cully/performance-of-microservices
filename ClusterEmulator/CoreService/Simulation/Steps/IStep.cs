7b7198898cdf8418d701ab6a0bcc977b30fe8301
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-07-27 19:29:15 +0100

    Updated comments

--- ClusterEmulator/CoreService/Simulation/Steps/IStep.cs
+++ ClusterEmulator/CoreService/Simulation/Steps/IStep.cs
@@ -3,8 +3,15 @@ using System.Threading.Tasks;
 
 namespace CoreService.Simulation.Steps
 {
+    /// <summary>
+    /// Interface for all execution steps.
+    /// </summary>
     public interface IStep
     {
+        /// <summary>
+        /// Executes the action defined by the step.
+        /// </summary>
+        /// <returns>A <see cref="ExecutionStatus"/> value.</returns>
         Task<ExecutionStatus> ExecuteAsync();
     }
 }
