c25d97ee3bbcc740a61b21458354f33a09c706f9
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-07-31 19:58:22 +0100

    AB#41 Add tests and validation to RequestStep

--- ClusterEmulator/CoreService.Test/Simulation/Steps/RequestStepUnitTests.cs
+++ ClusterEmulator/CoreService.Test/Simulation/Steps/RequestStepUnitTests.cs
@@ -12,9 +12,8 @@ namespace CoreService.Test.Simulation.Steps
         [TestMethod]
         public void Deserialization_ValidData_CreatesValidInstance()
         {
-            ulong expectedBytes = 0;
             RequestStep step = JsonConvert.DeserializeObject<RequestStep>(
-                "{ url: http://localhost/, size : 10, timeout: 3, retries : 0, retryPloicy : None }");
+                "{ method : GET, url : http://localhost/, size : 10, timeout: 3, retries : 0, retryPloicy : None }");
 
             Assert.AreEqual("http://localhost/", step.Url);
             Assert.AreEqual(10, step.PayloadSize);
@@ -33,7 +32,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_MissingUrl_Throws()
         {
             var step = new RequestStep()
-            { Url = string.Empty, PayloadSize = 15, Timeout = 2, Retries = 1, RetryPolicy = None };
+            { Method = "GET", Url = string.Empty, PayloadSize = 15, Timeout = 2, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -44,7 +43,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_FileUrl_Throws()
         {
             var step = new RequestStep()
-            { Url = "file://test.txt", PayloadSize = 15, Timeout = 2, Retries = 1, RetryPolicy = None };
+            { Method = "GET", Url = "file://test.txt", PayloadSize = 15, Timeout = 2, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -55,7 +54,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_RelativeUrl_Throws()
         {
             var step = new RequestStep()
-            { Url = "/test.html", PayloadSize = 15, Timeout = 2, Retries = 1, RetryPolicy = None };
+            { Method = "GET", Url = "/test.html", PayloadSize = 15, Timeout = 2, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -66,7 +65,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_InvalidPayloadSize_Throws()
         {
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = -1, Timeout = 1, Retries = 1, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", PayloadSize = -1, Timeout = 1, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -77,7 +76,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_MissingPayloadSize_Throws()
         {
             var step = new RequestStep()
-            { Url = "http://test.html", Timeout = 15, Retries = 1, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", Timeout = 15, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -88,7 +87,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_InvalidTimeout_Throws()
         {
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = 15, Timeout = -1, Retries = 1, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", PayloadSize = 15, Timeout = -1, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -99,7 +98,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_MissingTimeout_Throws()
         {
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = 15, Retries = 1, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", PayloadSize = 15, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -110,7 +109,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_InvalidRetries_Throws()
         {
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = 15, Timeout = 1, Retries = -1, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", PayloadSize = 15, Timeout = 1, Retries = -1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -121,7 +120,7 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_MissingRetries_Throws()
         {
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = 15, Timeout = 1, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", PayloadSize = 15, Timeout = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -132,7 +131,29 @@ namespace CoreService.Test.Simulation.Steps
         public async Task ExecuteAsync_MissingRetryPolicy_Throws()
         {
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = 15, Timeout = 1, Retries = 1 };
+            { Method = "GET", Url = "http://test.html", PayloadSize = 15, Timeout = 1, Retries = 1 };
+
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(
+                () => step.ExecuteAsync());
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MissingMethod_Throws()
+        {
+            var step = new RequestStep()
+            { Url = "http://test.html", PayloadSize = 15, Timeout = 1, Retries = 1, RetryPolicy = None };
+
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(
+                () => step.ExecuteAsync());
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MissingMethod_Throws()
+        {
+            var step = new RequestStep()
+            { Method = "FRY", Url = "http://test.html", PayloadSize = 15, Timeout = 1, Retries = 1, RetryPolicy = None };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -144,7 +165,7 @@ namespace CoreService.Test.Simulation.Steps
         {
             // TODO: mock endpoint
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
 
             ExecutionStatus status = await step.ExecuteAsync();
             Assert.AreEqual(ExecutionStatus.Success, status);
@@ -156,11 +177,84 @@ namespace CoreService.Test.Simulation.Steps
         {
             // TODO: mock endpoint
             var step = new RequestStep()
-            { Url = "http://test.html", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+            { Method = "GET", Url = "http://test.html", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
 
             ExecutionStatus status = await step.ExecuteAsync();
             Assert.AreEqual(ExecutionStatus.Fail, status);
         }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MethodDelete_CallsDelete()
+        {
+            // TODO: mock endpoint
+            var step = new RequestStep()
+            { Method = "DELETE", Url = "http://test/name", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+
+            ExecutionStatus status = await step.ExecuteAsync();
+            Assert.AreEqual(ExecutionStatus.Success, status);
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MethodHead_CallsHead()
+        {
+            // TODO: mock endpoint
+            var step = new RequestStep()
+            { Method = "HEAD", Url = "http://test/name", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+
+            ExecutionStatus status = await step.ExecuteAsync();
+            Assert.AreEqual(ExecutionStatus.Success, status);
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MethodOptions_CallsOptions()
+        {
+            // TODO: mock endpoint
+            var step = new RequestStep()
+            { Method = "OPTIONS", Url = "http://test/name", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+
+            ExecutionStatus status = await step.ExecuteAsync();
+            Assert.AreEqual(ExecutionStatus.Success, status);
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MethodPost_CallsPost()
+        {
+            // TODO: mock endpoint
+            var step = new RequestStep()
+            { Method = "post", Url = "http://test/name", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+
+            ExecutionStatus status = await step.ExecuteAsync();
+            Assert.AreEqual(ExecutionStatus.Success, status);
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MethodPut_CallsPut()
+        {
+            // TODO: mock endpoint
+            var step = new RequestStep()
+            { Method = "PUT", Url = "http://test/name", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+
+            ExecutionStatus status = await step.ExecuteAsync();
+            Assert.AreEqual(ExecutionStatus.Success, status);
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_MethodTrace_CallsTrace()
+        {
+            // TODO: mock endpoint
+            var step = new RequestStep()
+            { Method = "TRACE", Url = "http://test/name", PayloadSize = 15, Timeout = 0, Retries = 0, RetryPolicy = None };
+
+            ExecutionStatus status = await step.ExecuteAsync();
+            Assert.AreEqual(ExecutionStatus.Success, status);
+        }
+
 
         // TODO: add UTs with complex timeout and retry policies
     }
--- ClusterEmulator/CoreService/Simulation/Steps/RequestStep.cs
+++ ClusterEmulator/CoreService/Simulation/Steps/RequestStep.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -11,6 +12,14 @@ namespace CoreService.Simulation.Steps
     public class RequestStep : IStep
     {
         /// <summary>
+        /// The http protocol.
+        /// </summary>
+        [JsonProperty("method")]
+        [JsonRequired]
+        public string Method { get; set; }
+
+
+        /// <summary>
         /// The request url.
         /// </summary>
         [JsonProperty("url")]
@@ -18,9 +27,35 @@ namespace CoreService.Simulation.Steps
         public string Url { get; set; }
 
 
+        /// <summary>
+        /// The request payload size.
+        /// </summary>
+        [JsonProperty("size")]
+        [JsonRequired]
+        [Range(0, int.MaxValue, ErrorMessage = "size must be greater than 0")]
+        public int PayloadSize { get; set; }
+
+
         public async Task<ExecutionStatus> ExecuteAsync()
         {
             // TODO: if dealing with responses >50MB, implement streaming
+
+            // TODO: unify sizes to be in bytes
+
+            if (!UrlIsValid())
+            {
+                throw new InvalidOperationException("url must be a valid absolute URI");
+            }
+
+            if (string.IsNullOrWhiteSpace(Method) || !supportedMethods.Contains(Method, StringComparer.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("url must be a valid http protocol");
+            }
+
+            if (PayloadSize < 0)
+            {
+                throw new InvalidOperationException("size must be greater than 0");
+            }
 
             // TODO: add caller identifier to request
 
@@ -41,18 +76,40 @@ namespace CoreService.Simulation.Steps
         }
 
 
+        private bool UrlIsValid()
+        {
+            bool valid = false;
+            if (!string.IsNullOrWhiteSpace(Url))
+            {
+                try
+                {
+                    var uri = new UriBuilder(Url).Uri;
+                    valid = uri.IsAbsoluteUri;
+                }
+#pragma warning disable CA1031 // Do not catch general exception types
+                catch (UriFormatException)
+                {
+                    valid = false;
+                }
+#pragma warning restore CA1031 // Do not catch general exception types
+            }
+
+            return valid;
+        }
+
+
         /// <summary>
         /// Enumerable of supported Http methods in uppercase
         /// </summary>
         private readonly IEnumerable<string> supportedMethods = new List<HttpMethod>()
         {
+            HttpMethod.Delete,
             HttpMethod.Get,
+            HttpMethod.Head,
+            HttpMethod.Options,
             HttpMethod.Post,
             HttpMethod.Put,
-            HttpMethod.Head,
-            HttpMethod.Delete,
             HttpMethod.Trace,
-            HttpMethod.Options
         }.Select(m => m.Method.ToUpperInvariant());
     }
 }
