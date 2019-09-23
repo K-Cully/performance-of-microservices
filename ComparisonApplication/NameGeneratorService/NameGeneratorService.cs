0157dacc3bd5b4813b17c65f5300bae332605f31
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-23 09:47:16 +0100

    AB#13 Add registration of processor

--- ComparisonApplication/NameGeneratorService/NameGeneratorService.cs
+++ ComparisonApplication/NameGeneratorService/NameGeneratorService.cs
@@ -11,6 +11,7 @@ using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 using Microsoft.ServiceFabric.Data;
+using NameGeneratorService.Core;
 
 namespace NameGeneratorService
 {
@@ -40,7 +41,8 @@ namespace NameGeneratorService
                                     .UseKestrel()
                                     .ConfigureServices(
                                         services => services
-                                            .AddSingleton<StatelessServiceContext>(serviceContext))
+                                            .AddSingleton<StatelessServiceContext>(serviceContext)
+                                            .AddScoped<INameProcessor, NameProcessor>())
                                     .UseContentRoot(Directory.GetCurrentDirectory())
                                     .UseStartup<Startup>()
                                     .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
