6ec13d5afb25a4934bc783b1d80d1dbe71ddabe2
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-23 10:54:32 +0100

    AB#13 Add random generator service

---
+++ ComparisonApplication/RandomGeneratorService/Controllers/RandomController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomGeneratorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        // GET api/random
+        [HttpGet]
+        public async Task<ActionResult<int>> GetAsync()
+        {
+            // TODO: implement
+            return await Task.FromResult(1);
+        }
+    }
+}
