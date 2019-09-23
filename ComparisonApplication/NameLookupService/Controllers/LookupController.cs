17d054d063c3f88c37cc5a9f831c991d1fafdc83
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-23 12:25:41 +0100

    AB#13 Add name lookup service shell

---
+++ ComparisonApplication/NameLookupService/Controllers/LookupController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NameLookupService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LookupController : ControllerBase
+    {
+        // GET api/lookup/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync(int id)
+        {
+            // TODO: implement
+            return new List<string>();
+        }
+
+
+        // POST api/lookup
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<string>>> PostAsync([FromBody] IEnumerable<int> ids)
+        {
+            // TODO: implement
+            return new List<string>();
+        }
+    }
+}
