c8566756dd3cb33d56ef10148f84248d43f153fb
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-23 16:25:08 +0100

    AB#13 Update .net core version references

--- ComparisonApplication/NameGeneratorService/Startup.cs
+++ ComparisonApplication/NameGeneratorService/Startup.cs
@@ -20,7 +20,7 @@ namespace NameGeneratorService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // Add http clients to the default factory
             services.AddHttpClient(Settings.RandomApiClientName,
