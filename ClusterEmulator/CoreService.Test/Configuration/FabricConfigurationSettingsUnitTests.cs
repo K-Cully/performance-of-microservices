c64057143f1736a14fb10bac0dc5e2ad43d0a96e
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-25 12:19:30 +0100

    AB#10 Add registry setting fiels and Unit tests

---
+++ ClusterEmulator/CoreService.Test/Configuration/FabricConfigurationSettingsUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using ClusterEmulator.Service.Simulation.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace CoreService.Test.Controllers
+{
+    [TestClass]
+    public class FabricConfigurationSettingsUnitTests
+    {
+        [TestMethod]
+        [TestCategory("Input")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_Throws_WhenPassedNullLogger()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
---
+++ ClusterEmulator/Service.Simulation/Core/IRegistrySettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ClusterEmulator.Service.Simulation.Core
+{
+    /// <summary>
+    /// Interface to facilitate flexible registry setting loading
+    /// </summary>
+    public interface IRegistrySettings
+    {
+        /// <summary>
+        /// Attempts to retrieve the enumeration of settings from a configuration section
+        /// </summary>
+        /// <param name="name">The name of the section to retrieve.</param>
+        /// <param name="section">The settings in name, value format.</param>
+        /// <returns>True if section could be retrieved, false otherwise.</returns>
+        bool TryGetSection(string name, out IEnumerable<KeyValuePair<string, string>> section);
+    }
+}
