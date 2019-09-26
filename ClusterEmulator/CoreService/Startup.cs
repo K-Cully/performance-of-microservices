2f817bab58728e5b2af11b68d4fb7d0f4df2fed2
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-26 10:52:03 +0100

    AB#10 Clean up comments

--- ClusterEmulator/CoreService/Startup.cs
+++ ClusterEmulator/CoreService/Startup.cs
@@ -9,18 +9,28 @@ using System.Net.Http;
 
 namespace CoreService
 {
+    /// <summary>
+    /// Provides an ease-of-use abstraction for instance container managment and http pipeline configuration.
+    /// </summary>
     public class Startup
     {
         private IRegistry Registry { get; }
 
 
+        /// <summary>
+        /// Creates a new instance of <see cref="Startup"/>
+        /// </summary>
+        /// <param name="registry">The simulation registry</param>
         public Startup(IRegistry registry)
         {
             Registry = registry ?? throw new ArgumentNullException(nameof(registry));
         }
 
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime and is used to add services to the container.
+        /// </summary>
+        /// <param name="services">The service container.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -28,7 +38,13 @@ namespace CoreService
         }
 
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime and is used to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">Environemnt information wrapper.</param>
+        /// <param name="clientFactory">A factory for creating Http Clients</param>
+        /// <param name="registry">The simulation registry containing configuration information.</param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IHttpClientFactory clientFactory, IRegistry registry)
         {
             _ = app ?? throw new ArgumentNullException(nameof(app));
