2fbb76c76bbe8b5824b66f4c1339902ba0bca364
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-26 11:40:05 +0100

    AB#10 Update Program.cs files

--- ClusterEmulator/RandomGeneratorService/Program.cs
+++ ClusterEmulator/RandomGeneratorService/Program.cs
@@ -1,13 +1,34 @@
+using ClusterEmulator.Service.Shared.Telemetry;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
 using Microsoft.ServiceFabric.Services.Runtime;
+using Serilog;
+using Serilog.Core;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace RandomGeneratorService
 {
     internal static class Program
     {
+        private const string ASPNETCORE_ENVIRONMENT = "ASPNETCORE_ENVIRONMENT";
+
+        private const string ServiceTypeName = "RandomGeneratorServiceType";
+
+
+        /// <summary>
+        /// App settings for use in log configuration
+        /// </summary>
+        private static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable(ASPNETCORE_ENVIRONMENT) ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+
         /// <summary>
         /// This is the entry point of the service host process.
         /// </summary>
@@ -15,23 +36,33 @@ namespace RandomGeneratorService
         {
             try
             {
-                // The ServiceManifest.XML file defines one or more service type names.
-                // Registering a service maps a service type name to a .NET type.
-                // When Service Fabric creates an instance of this service type,
-                // an instance of the class is created in this host process.
+                // TODO: set App Insights key from external source
+                var telemetry = new TelemetryConfiguration("");
+                Logger log = new LoggerConfiguration()
+                                .ReadFrom.Configuration(Configuration)
+                                .Enrich.FromLogContext()
+                                .WriteTo.ApplicationInsights(telemetry, new OperationTelemetryConverter())
+                                .CreateLogger();
 
-                ServiceRuntime.RegisterServiceAsync("RandomGeneratorServiceType",
-                    context => new RandomGeneratorService(context)).GetAwaiter().GetResult();
+                // Create service instance
+                ServiceRuntime.RegisterServiceAsync(ServiceTypeName,
+                    context => new RandomGeneratorService(context, log)).GetAwaiter().GetResult();
 
-                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(RandomGeneratorService).Name);
+                Log.Information("Service registered - {ProcessId}, {ServiceId}",
+                    Process.GetCurrentProcess().Id, ServiceTypeName);
 
-                // Prevents this host process from terminating so services keeps running. 
+                // Prevents this host process from terminating so services keep running. 
                 Thread.Sleep(Timeout.Infinite);
             }
             catch (Exception e)
             {
-                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                Log.Fatal(e, "Failed to initialize service {ServiceId}", ServiceTypeName);
                 throw;
+            }
+            finally
+            {
+                // Clean up logger if process terminates
+                Log.CloseAndFlush();
             }
         }
     }
