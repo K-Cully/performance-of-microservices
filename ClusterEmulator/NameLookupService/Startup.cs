0ed35294db9484320446c66661e4cb42ec6495d8
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-26 11:42:21 +0100

    AB#10 Update Startup.cs files

--- ClusterEmulator/NameLookupService/Startup.cs
+++ ClusterEmulator/NameLookupService/Startup.cs
@@ -1,34 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ClusterEmulator.Service.Simulation.Core;
+using ClusterEmulator.Service.Simulation.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 
 namespace NameLookupService
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private IRegistry Registry { get; }
+
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Startup"/>
+        /// </summary>
+        /// <param name="registry">The simulation registry</param>
+        public Startup(IRegistry registry)
         {
+            Registry = registry ?? throw new ArgumentNullException(nameof(registry));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+
+        /// <summary>
+        /// This method gets called by the runtime and is used to add services to the container.
+        /// </summary>
+        /// <param name="services">The service container.</param>
+        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSimulationEngineClients(Registry);
+        }
+
+
+        /// <summary>
+        /// This method gets called by the runtime and is used to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">Environemnt information wrapper.</param>
+        /// <param name="clientFactory">A factory for creating Http Clients</param>
+        /// <param name="registry">The simulation registry containing configuration information.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IHttpClientFactory clientFactory, IRegistry registry)
+        {
+            _ = app ?? throw new ArgumentNullException(nameof(app));
+            _ = env ?? throw new ArgumentNullException(nameof(env));
+            _ = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
+            _ = registry ?? throw new ArgumentNullException(nameof(registry));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Injecting registry here to ensure it is added from the second, more locally scoped, instance container.
+            registry.ConfigureHttpClients(clientFactory);
+            app.UseMvc();
         }
     }
 }
