a595d4eb11691eb1c119a80e39cca5d8f4ffd957
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-09-27 10:05:49 +0100

    AB#44 Update load step  to allow specification of processor count

--- ClusterEmulator/Service.Simulation.Test/Steps/LoadStepUnitTests.cs
+++ ClusterEmulator/Service.Simulation.Test/Steps/LoadStepUnitTests.cs
@@ -12,14 +12,28 @@ namespace ClusterEmulator.Service.Simulation.Test.Steps
     public class LoadStepUnitTests
     {
         [TestMethod]
-        public void Deserialization_ValidData_CreatesValidInstance()
+        public void Deserialization_RequiredData_CreatesValidInstance()
         {
             ulong expectedBytes = 0;
-            LoadStep step = JsonConvert.DeserializeObject<LoadStep>("{ bytes: 0, time : 10, percent : 20 }");
+            LoadStep step = JsonConvert.DeserializeObject<LoadStep>("{ bytes: 0, time : 10.2, percent : 20 }");
 
             Assert.AreEqual(20, step.CpuPercentage);
-            Assert.AreEqual(10, step.TimeInSeconds);
+            Assert.AreEqual(10.2d, step.TimeInSeconds, 0.0001d);
             Assert.AreEqual(expectedBytes, step.MemoryInBytes);
+            Assert.AreEqual(0, step.MaxProcessors);
+        }
+
+
+        [TestMethod]
+        public void Deserialization_AllData_CreatesValidInstance()
+        {
+            ulong expectedBytes = 0;
+            LoadStep step = JsonConvert.DeserializeObject<LoadStep>("{ bytes: 0, time : 10, percent : 20, processors : 2 }");
+
+            Assert.AreEqual(20, step.CpuPercentage);
+            Assert.AreEqual(10.0d, step.TimeInSeconds, 0.0001d);
+            Assert.AreEqual(expectedBytes, step.MemoryInBytes);
+            Assert.AreEqual(2, step.MaxProcessors);
         }
 
 
@@ -35,7 +49,7 @@ namespace ClusterEmulator.Service.Simulation.Test.Steps
         public async Task ExecuteAsync_LoggerNotInitialized_Throws()
         {
             var step = new LoadStep()
-            { MemoryInBytes = 2, TimeInSeconds = 15, CpuPercentage = 2 };
+            { MemoryInBytes = 2, TimeInSeconds = 5, CpuPercentage = 2 };
 
             await Assert.ThrowsExceptionAsync<InvalidOperationException>(
                 () => step.ExecuteAsync());
@@ -46,7 +60,7 @@ namespace ClusterEmulator.Service.Simulation.Test.Steps
         public async Task ExecuteAsync_InvalidPercent_Throws()
         {
             var step = new LoadStep()
-            { MemoryInBytes = 2, TimeInSeconds = 15, CpuPercentage = -100 };
+            { MemoryInBytes = 2, TimeInSeconds = 15.1d, CpuPercentage = -100, };
             var logger = new Mock<ILogger>(MockBehavior.Loose);
             step.InitializeLogger(logger.Object);
 
@@ -59,7 +73,20 @@ namespace ClusterEmulator.Service.Simulation.Test.Steps
         public async Task ExecuteAsync_InvalidTime_Throws()
         {
             var step = new LoadStep()
-            { MemoryInBytes = 5, TimeInSeconds = -15, CpuPercentage = 5 };
+            { MemoryInBytes = 5, TimeInSeconds = -15.0d, CpuPercentage = 5 };
+            var logger = new Mock<ILogger>(MockBehavior.Loose);
+            step.InitializeLogger(logger.Object);
+
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(
+                () => step.ExecuteAsync());
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_InvalidMaxProcessors_Throws()
+        {
+            var step = new LoadStep()
+            { MemoryInBytes = 5, TimeInSeconds = 2.0d, CpuPercentage = 5, MaxProcessors = -1 };
             var logger = new Mock<ILogger>(MockBehavior.Loose);
             step.InitializeLogger(logger.Object);
 
@@ -73,14 +100,31 @@ namespace ClusterEmulator.Service.Simulation.Test.Steps
         {
             var start = DateTime.UtcNow;
             var step = new LoadStep()
-            { MemoryInBytes = 10, TimeInSeconds = 1, CpuPercentage = 10 };
+            { MemoryInBytes = 10, TimeInSeconds = 0.2d, CpuPercentage = 10 };
             var logger = new Mock<ILogger>(MockBehavior.Loose);
             step.InitializeLogger(logger.Object);
 
             ExecutionStatus status = await step.ExecuteAsync();
             var timeSpan = DateTime.UtcNow.Subtract(start);
 
-            Assert.IsTrue(timeSpan.TotalSeconds >= 1.0d);
+            Assert.IsTrue(timeSpan.TotalSeconds >= 0.2d);
+            Assert.AreEqual(ExecutionStatus.Success, status);
+        }
+
+
+        [TestMethod]
+        public async Task ExecuteAsync_ProcessorCountOverriden_ExecutesCorrectly()
+        {
+            var start = DateTime.UtcNow;
+            var step = new LoadStep()
+            { MemoryInBytes = 10, TimeInSeconds = 0.2d, CpuPercentage = 10, MaxProcessors = 1 };
+            var logger = new Mock<ILogger>(MockBehavior.Loose);
+            step.InitializeLogger(logger.Object);
+
+            ExecutionStatus status = await step.ExecuteAsync();
+            var timeSpan = DateTime.UtcNow.Subtract(start);
+
+            Assert.IsTrue(timeSpan.TotalSeconds >= 0.2d);
             Assert.AreEqual(ExecutionStatus.Success, status);
         }
 
@@ -90,7 +134,7 @@ namespace ClusterEmulator.Service.Simulation.Test.Steps
         {
             var start = DateTime.UtcNow;
             var step = new LoadStep()
-            { MemoryInBytes = 1, TimeInSeconds = 1, CpuPercentage = 1 };
+            { MemoryInBytes = 1, TimeInSeconds = 1.0d, CpuPercentage = 1 };
             var logger = new Mock<ILogger>(MockBehavior.Loose);
             step.InitializeLogger(logger.Object);
 
@@ -106,7 +150,7 @@ namespace ClusterEmulator.Service.Simulation.Test.Steps
         public async Task ExecuteAsync_ValidBytesMax_Throws()
         {
             var step = new LoadStep()
-            { MemoryInBytes = ulong.MaxValue, TimeInSeconds = 15, CpuPercentage = 50 };
+            { MemoryInBytes = ulong.MaxValue, TimeInSeconds = 2, CpuPercentage = 50 };
             var logger = new Mock<ILogger>(MockBehavior.Loose);
             step.InitializeLogger(logger.Object);
 
