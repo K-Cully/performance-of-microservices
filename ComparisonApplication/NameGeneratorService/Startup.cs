9c7ed8b30abf0714e6b1e97104b8c549f5bc40b4
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-10-03 10:40:00 +0100

    AB#14 Update logging in comparison application

--- ComparisonApplication/NameGeneratorService/Startup.cs
+++ ComparisonApplication/NameGeneratorService/Startup.cs
@@ -4,29 +4,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NameGeneratorService.Core;
 
 namespace NameGeneratorService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
         {
-            Configuration = configuration;
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public IConfiguration Configuration { get; }
+
+        public ILogger<Startup> Logger;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            string randomUrl = Settings.RandomServiceBaseUrl;
+            string lookupUrl = Settings.LookupServiceBaseUrl;
+
+            Logger.LogInformation("Resolved {RandomServiceUrl} for random generator http client", randomUrl);
+            Logger.LogInformation("Resolved {LookupServiceUrl} for name lookup http client", lookupUrl);
+
             // Add http clients to the default factory
             services.AddHttpClient(Settings.RandomApiClientName,
-                c => c.BaseAddress = new Uri(Settings.RandomServiceBaseUrl));
+                c => c.BaseAddress = new Uri(randomUrl));
             services.AddHttpClient(Settings.NameLookupApiClientName,
-                c => c.BaseAddress = new Uri(Settings.LookupServiceBaseUrl));
+                c => c.BaseAddress = new Uri(lookupUrl));
 
             services.AddHealthChecks();
         }
