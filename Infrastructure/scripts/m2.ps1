0700d00da38f4c23bc8189a8ec9823e3cd561bfc
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-10-10 13:39:34 +0100

    AB#46 Update ARM template and script to deploy a basic single node cluster

--- Infrastructure/scripts/m2.ps1
+++ Infrastructure/scripts/m2.ps1
@@ -2,18 +2,44 @@ param(
     [string] [Parameter(Mandatory = $true)] $Name
 )
 
+# Add utility methods
 . "$PSScriptRoot\Common.ps1"
 
+# Define general control variables
 $ResourceGroupName = "$Name-rg"
 $Location = "North Europe"
 $KeyValutName = "$Name-vault"
 
+# Check that you're logged in to Azure before running anything at all, the call will
+# exit the script if you're not
 CheckLoggedIn
+
+# Check the resource group we are deploying to exists or create it
 EnsureResourceGroup $ResourceGroupName $Location
 
+# Check that the Key Vault resource exists or crete it
 $keyVault = EnsureKeyVault $KeyValutName $ResourceGroupName $Location
 
+# Create a self-signed cluster certificate for development and testing 
 $certThumbprint, $certPassword, $certPath = CreateSelfSignedCertificate $Name
+
+# Add the certificate to the Key Vault
 $keyVaultCert = ImportCertificateIntoKeyVault $KeyValutName $Name $certPath $certPassword
 
-$keyVaultCert
+# Create parameters for cluster resource deployment
+Write-Host "Deploying cluster with ARM template..."
+$armParameters = @{
+    namePart = $Name;
+    certificateThumbprint = $certThumbprint;
+    sourceVaultResourceId = $keyVault.ResourceId;
+    certificateUrlValue = $keyVaultCert.SecretId;
+    rdpPassword = GeneratePassword;
+}
+
+# Create cluster resources based on the specified ARM template
+New-AzureRmResourceGroupDeployment `
+  -ResourceGroupName $ResourceGroupName `
+  -TemplateFile "$PSScriptRoot\minimal.json" `
+  -Mode Incremental `
+  -TemplateParameterObject $armParameters `
+  -Verbose
