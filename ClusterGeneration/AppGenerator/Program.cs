ee057886b21d39c407c17762eedf7321f2863477
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-10-15 09:58:45 +0100

    AB#53 Add skeleton of app generator tool

---
+++ ClusterGeneration/AppGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
