5913cd461702d3a009e83c68c3cbcb42302052b0
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-10-15 12:36:09 +0100

    AB#53 process config as hashtable

--- ClusterGeneration/AppGenerator.ps1
+++ ClusterGeneration/AppGenerator.ps1
@@ -1,37 +1,82 @@
-# Notes: Requires powershell 6
+# Note: Requires Powershell Core 6+
 
 param(
     [string] [Parameter(Mandatory = $true)] $Name,
     [string] [Parameter(Mandatory = $true)] $ConfigFile
 )
- 
-if (-Not (Test-Path -Path $ConfigFile)) {
-    Write-Error -Message "File $ConfigFile not found"
-    exit 1
-}
 
-$config = Get-Content $ConfigFile | ConvertFrom-Json
-if ((-Not $config.services) -or $config.services.Count -eq 0)
+function Read-AppConfig([string]$Path)
 {
-    Write-Error -Message "No services are sepcified in $ConfigFile"
-    exit 1
+    if (-Not (Test-Path -Path $Path)) {
+        Write-Error -Message "File $Path not found"
+        exit 1
+    }
+    
+    $config = Get-Content $Path | ConvertFrom-Json -AsHashtable
+    if ((-Not $config.services) -or $config.services.Count -eq 0)
+    {
+        Write-Error -Message "No services are sepcified in $Path"
+        exit 1
+    }
+
+    $config
 }
 
+
+function Validate-ServiceConfig([string]$Config)
+{
+    if (-Not $Config.port) {
+        Write-Error -Message "port is missing from service $serviceName"
+        exit 1
+    }
+    elseif($usedPorts[$Config.port]) {
+        $port = $Config.port
+        $otherService = $usedPorts[$port]
+        Write-Error -Message "port $port is already used by $otherService, cannot create $serviceName"
+        exit 1
+    }
+    elseif ((-Not $Config.processors) -or $Config.processors.Count -eq 0) {
+        Write-Error -Message "processors are missing from service $serviceName"
+        exit 1
+    }
+    elseif ((-Not $Config.steps) -or $Config.steps.Count -eq 0) {
+        Write-Error -Message "steps are missing from service $serviceName"
+        exit 1
+    }
+}
+
+
+$usedPorts = @{}
+
+$config = Read-AppConfig -Path $ConfigFile 
+
+$appSettingsFile = $null
 if ($config.settingsPath) {
     if (-Not (Test-Path -Path $config.settingsPath)) {
         Write-Error -Message "Settings path $ConfigFile is not valid"
         exit 1
     }
 
-    # TODO: copy settings after template generation        
+    $appSettingsFile = $config.settingsPath      
 }
-elseif ($null -ne $config.aiKey) {
+elseif ($config.aiKey) {
     # TODO: pass to template generation
 }
 
-foreach ($service in $config.services) {
-    if (-Not $service.port) {
-        Write-Error -Message "Service path $ConfigFile is not valid"
-        exit 1
+foreach ($serviceName in $config.services.Keys) {
+    $serviceConfig = $config.services.$serviceName
+    Validate-ServiceConfig -Config $serviceConfig
+
+    # Add port to used table
+    $usedPorts[$serviceConfig.port] = $serviceName
+
+    # TODO: call dotnet new for templated service with aiKey
+    
+    if ($appSettingsFile)
+    {
+        # TODO: replace the appsettings.json file after project genreration
     }
+
+    
+    # TODO: optionally process clients and policies ()
 }
