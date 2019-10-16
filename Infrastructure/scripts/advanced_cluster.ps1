ad1bd690d42869125846ef5c64f0ae656ecf1867
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-10-16 16:30:00 +0100

    AB#55 Enable service port forwarding rule creation

--- Infrastructure/scripts/advanced_cluster.ps1
+++ Infrastructure/scripts/advanced_cluster.ps1
@@ -32,48 +32,39 @@ $keyVault = EnsureKeyVault $KeyVaultName $ResourceGroupName $Location
 # Ensure that a self-signed certificate is created and imported into Key Vault
 $cert = EnsureSelfSignedCertificate $KeyVaultName $Name
 
-
-if ($PortMappingFile) {
-  # Create an array of service parameters
-  $serviceObjects = foreach ($port in $a.Keys) {
-    $obj = New-Object -TypeName psobject
-    $obj | Add-Member -MemberType NoteProperty -Name name -Value $a[$port]
-    $obj | Add-Member -MemberType NoteProperty -Name port -Value $porttion
-    $obj
-  }
-
-  # Convert the array to JSON
-  $serviceJson = $serviceObjects | ConvertTo-Json
-
-  # TODO: pass Json to template
-
-  # TODO: generate JSON with Names and Ports in an acceptable format 
-  # TODO: update ports and health checks in load balancer resource
-
+# Create parameters for cluster resource deployment
+$armParameters = @{
+  namePart = $Name;
+  certificateThumbprint = $cert.Thumbprint;
+  sourceVaultResourceId = $keyVault.ResourceId;
+  certificateUrlValue = $cert.SecretId;
+  rdpPassword = GeneratePassword;
+  vmInstanceCount = $NodeCount;
+  reliability = $ClusterTier;
+  durability = $DurabilityLevel;
+  omsWorkspaceName = $OmsWorkspaceName;    
 }
 
+if ($PortMappingFile) {
+  # Create Service Fabric managment load balancer rules
+  [array]$rules = @{name="FabricManagement"; frontendPort="19000"; backendPort="19000"; protocol="tcp";probeProtocol="tcp";probePath=$null}
+  [array]$rules += @{name="FabricExplorer"; frontendPort="19080"; backendPort="19080"; protocol="tcp";probeProtocol="tcp";probePath=$null}
 
-# Create parameters for cluster resource deployment
-Write-Host "Applying cluster template $TemplateName..."
-$armParameters = @{
-    namePart = $Name;
-    certificateThumbprint = $cert.Thumbprint;
-    sourceVaultResourceId = $keyVault.ResourceId;
-    certificateUrlValue = $cert.SecretId;
-    rdpPassword = GeneratePassword;
-    vmInstanceCount = $NodeCount;
-    reliability = $ClusterTier;
-    durability = $DurabilityLevel;
-    omsWorkspaceName = $OmsWorkspaceName;
+  # Read services and ports
+  $portServiceMap = Get-Content -Path $PortMappingFile | ConvertFrom-Json -AsHashtable
+
+  # Add service load balancer rules
+  $serviceObjects = foreach ($port in $portServiceMap.Keys) {
+    $serviceName = $portServiceMap[$port]
+    [array]$rules += @{name="$serviceName"; frontendPort="$port"; backendPort="$port"; protocol="tcp";probeProtocol="http";probePath="/health"}
+  }
+
+  $armParameters.lbRules = $rules;
 }
 
 # Create cluster resources based on the specified ARM template
-$creationResult = New-AzureRmResourceGroupDeployment `
-  -ResourceGroupName $ResourceGroupName `
-  -TemplateFile "$TemplateDirectory\$TemplateName" `
-  -Mode Incremental `
-  -TemplateParameterObject $armParameters `
-  -Verbose
+Write-Host "Applying cluster template $TemplateName..."
+$creationResult = New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile "$TemplateDirectory\$TemplateName" -Mode Incremental -TemplateParameterObject $armParameters -Verbose
 
 # Add relevant performance counters to the OMS workspace
 Add-PerformanceCounters -ResourceGroup $ResourceGroupName -WorkspaceName $OmsWorkspaceName
