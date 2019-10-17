22f5ca51398be62ea9f8301fa44bd7501e2340db
K-Cully <2370032+K-Cully@users.noreply.github.com>
2019-10-17 10:58:03 +0100

    AB#57 make changes to core deployment script to unblock deployment with PS Core

--- Infrastructure/scripts/deploy_app_core.ps1
+++ Infrastructure/scripts/deploy_app_core.ps1
@@ -12,35 +12,78 @@ Import-Module -Name Microsoft.ServiceFabric.Powershell.Http
 . "$PSScriptRoot\Common.ps1"
 
 # Configure application values
-$applicationType = $ApplicationName + "Type"
-$applicationVersion = "1.0.0"
+$ApplicationType = $ApplicationName + "Type"
+$ApplicationVersion = "1.0.0"
 
 # Configure cluster connection
-$endpoint = "https://$ClusterName.$Location.cloudapp.azure.com:19080"
-$thumbprint = Get-Content "$PSScriptRoot\$ClusterName.thumb.txt"
-
-# TODO: Note demo had the endpoint port as 19080 
+$Endpoint = "https://$ClusterName.$Location.cloudapp.azure.com:19080"
+$Thumbprint = Get-Content "$PSScriptRoot\$ClusterName.thumb.txt"
+$TimeoutSeconds = 600
 
 # Connect to cluster
-Write-Host "connecting to cluster $endpoint using cert thumbprint $thumbprint..."
-Connect-SFCluster -ConnectionEndpoint $endpoint `
+Write-Host "connecting to cluster $Endpoint using cert thumbprint $Thumbprint..."
+Connect-SFCluster -ConnectionEndpoint $Endpoint `
     -X509Credential `
-    -ServerCertThumbprint $thumbprint `
-    -FindType FindByThumbprint -FindValue $thumbprint `
+    -ServerCertThumbprint $Thumbprint `
+    -FindType FindByThumbprint -FindValue $Thumbprint `
     -StoreLocation CurrentUser -StoreName My
 
-Write-Host "Unregistering $applicationType if present..."
-$registeredTypes = Get-SFApplicationType
-if ($registeredTypes) {
-    # TODO: Get-SFApplicationType https://github.com/microsoft/service-fabric-client-dotnet/blob/develop/docs/cmdlets/Get-SFApplicationType.md
-    # TODO: Unregister-SFApplicationType -ApplicationTypeName $applicationType -ApplicationTypeVersion $applicationVersion
+# Stop active applications
+$activeApplications = Get-SFApplication -ApplicationTypeName $ApplicationType -ServerTimeout $TimeoutSeconds
+if ($activeApplications) {
+    Write-Host "Found $($activeApplications.Count) active applications matching $ApplicationType"
+    foreach ($app in $activeApplications) {
+        Write-Host "Stopping $($app.ApplicationId) version $($app.ApplicationTypeVersion)"
+        Remove-SFApplication -ApplicationId $app.ApplicationId -ForceRemove $true -ServerTimeout $TimeoutSeconds -Force
+    }
 }
 
-Write-Host "Uploading application package to the cluster image store..."
-Copy-SFApplicationPackage -ApplicationPackagePath $PackagePath -ApplicationPackagePathInImageStore $applicationType
+# Remove all application versions of the app type
+$registeredTypes = Get-SFApplicationType -ApplicationTypeName $ApplicationType -ServerTimeout $TimeoutSeconds
+if ($registeredTypes) {
+    Write-Host "Found $($registeredTypes.Count) registered application types matching $ApplicationType"
+    foreach ($application in $registeredTypes) {
+        Write-Host "Unregistering $($application.ApplicationTypeName) version $($application.ApplicationTypeVersion)"
+        Unregister-SFApplicationType -ApplicationTypeName $application.ApplicationTypeName `
+            -ApplicationTypeVersion $application.ApplicationTypeVersion -ServerTimeout $TimeoutSeconds -Force
+    }
+}
 
-Write-Host "Registering application $applicationType..."
-Register-SFApplicationType -ApplicationTypeBuildPath $applicationType
+# Remove app from the image store if it is already present
+$existingContent = $null
+try {
+    $existingContent = Get-SFImageStoreContent -ContentPath $ApplicationType -ErrorAction Ignore
+}
+catch {
+    # Swallow error since -ErrorAction Ignore is not honoured
+}
+
+if ($existingContent) {
+    Write-Host "Removing existing content from the image store."
+    Remove-SFImageStoreContent -ContentPath $ApplicationType -ServerTimeout $TimeoutSeconds -Force
+}
+
+# Since the module doesn't return anything useful to indicate failure reason, retry all errors
+$result = $null
+$attempt = 0
+while ($result -ne "Success!" -and $attempt -lt 5) {
+    $attempt = $attempt + 1
+    Write-Host "Uploading application package to the cluster image store. (Attempt $attempt)"
+    $result = powershell Copy-SFApplicationPackage -ApplicationPackagePath $PackagePath -ApplicationPackagePathInImageStore $ApplicationType
+}
+
+# Return with failure if package could not be uploaded
+if ($result -ne "Success!") {
+    Write-Error -Message "Upload failed with message '$result' after $attempt attempts!"
+    exit 1
+}
+
+Write-Host "Application package uploaded to image store after $attempt attempts."
+
+# Note, may need to make async and delay creation for large app sizes
+Write-Host "Registering application $ApplicationType..."
+Register-SFApplicationType -ImageStorePath -ApplicationTypeBuildPath $ApplicationType -ServerTimeout $TimeoutSeconds
 
 Write-Host "Creating application..."
-New-SFApplication -Name "fabric:/$applicationType" -TypeName $applicationType -TypeVersion $applicationVersion
+New-SFApplication -Name "fabric:/$ApplicationType" -TypeName $ApplicationType -TypeVersion $ApplicationVersion `
+    -ServerTimeout $TimeoutSeconds
